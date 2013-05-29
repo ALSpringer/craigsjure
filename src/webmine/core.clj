75682dd8d3ca1b418b906fff1e3f0bb553bc507e
Aaron Springer <Aaron@Aarons-MacBook-Air.local>
2013-05-28 22:50:11 -0400

    Switched from using elements to the more explicit find-elements

--- src/webmine/core.clj
+++ src/webmine/core.clj
@@ -2,11 +2,13 @@
   (:require [clj-webdriver.taxi :refer :all])
   (:gen-class :main true))
 
+(defstruct listing :title :price)
+
 (defn -main
   [& args]
     (set-driver! {:browser :firefox} "http://harrisonburg.craigslist.com/bia/")
-    (print (second (map text (elements ".row")))))
-
-
+;    (print (first (map text (attribute (find-elements {:css ".row"}) :data-pid))))
+    (print (first (map text (find-elements {:css ".row .pl a"}))))
+    (print (first (map text (find-elements {:css ".row .price"})))))
 
 
