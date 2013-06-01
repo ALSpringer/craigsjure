b78c836768b1513dfd8b6784e573d360211950a3
Aaron Springer <Aaron@Aarons-MacBook-Air.local>
2013-06-01 11:12:10 -0400

    Now retrieves top 10 entries from the bike list and spits them to a file for comparison

--- src/webmine/core.clj
+++ src/webmine/core.clj
@@ -1,14 +1,27 @@
 (ns webmine.core
   (:require [clj-webdriver.taxi :refer :all])
   (:gen-class :main true))
+(use 'clojure.java.io)
+(use 'clojure.pprint)
 
-(defstruct listing :title :price)
+(defn spitlist
+  [list]
+  (doseq [i list]
+    (spit "test.txt"
+          (str (attribute i "data-pid") " ") :append true)
+    (spit "test.txt"
+          (str (text (find-element-under i {:css ".pl a"})) " "):append true)
+    (spit "test.txt"
+         (str (text (find-element-under i {:css "span span span"})) "\n"):append true))
+    (print "Finished reading the top 10 bicycle entries"))
 
 (defn -main
   [& args]
-    (set-driver! {:browser :firefox} "http://harrisonburg.craigslist.com/bia/")
-;    (print (first (map text (attribute (find-elements {:css ".row"}) :data-pid))))
-    (print (first (map text (find-elements {:css ".row .pl a"}))))
-    (print (first (map text (find-elements {:css ".row .price"})))))
+    (set-driver! {:browser :firefox} "http://boston.craigslist.com/bia/")
+    (let [listings (find-elements {:css ".row"})]
+      (spitlist (take 10 listings))))
+    ;(attribute (first (find-elements {:css ".row"}))"data-pid"))
+    ;(print (first (map text (find-elements {:css ".row .pl a"}))))
+    ;(print (first (map text (find-elements {:css ".row .price"})))))
 
 
