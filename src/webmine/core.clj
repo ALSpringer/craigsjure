1968f4cd0761e07708a55591334106e31ba09efc
ALSpringer <aaronlspringer@gmail.com>
2013-06-02 15:22:06 -0400

    Added ability to read in output file for comparison

--- src/webmine/core.clj
+++ src/webmine/core.clj
@@ -2,25 +2,28 @@
   (:require [clj-webdriver.taxi :refer :all])
   (:gen-class :main true))
 (use 'clojure.java.io)
+(use '[clojure.string :only [split-lines]])
 
 (defn spitlist
   [list]
   (doseq [i list]
     (spit "test.txt"
-          (str (attribute i "data-pid") " ") :append true)
+          (str (attribute i "data-pid") ",") :append true)
     (spit "test.txt"
-          (str (text (find-element-under i {:css ".pl a"})) " "):append true)
+          (str (text (find-element-under i {:css ".pl a"})) ","):append true)
     (spit "test.txt"
          (str (text (find-element-under i {:css "span span span"})) "\n"):append true))
     (print "Finished reading the top 10 bicycle entries"))
+
+(defn readlist
+  [file]
+  (doseq [j (split-lines (slurp file))]
+    (prn j)))
 
 (defn -main
   [& args]
     (set-driver! {:browser :firefox} "http://boston.craigslist.com/bia/")
     (let [listings (find-elements {:css ".row"})]
       (spitlist (take 10 listings))))
-    ;(attribute (first (find-elements {:css ".row"}))"data-pid"))
-    ;(print (first (map text (find-elements {:css ".row .pl a"}))))
-    ;(print (first (map text (find-elements {:css ".row .price"})))))
 
 
