21a77975d510d72a6feed31afcf5efd3c1c3d41e
ALSpringer <aaronlspringer@gmail.com>
2013-06-02 17:50:39 -0400

    Added function to determine which posts are new

--- src/webmine/core.clj
+++ src/webmine/core.clj
@@ -1,29 +1,49 @@
 (ns webmine.core
   (:require [clj-webdriver.taxi :refer :all])
-  (:gen-class :main true))
-(use 'clojure.java.io)
-(use '[clojure.string :only [split-lines]])
+  (:gen-class :main true)
+  (:use [clojure.java.io]
+  [clojure.core]
+  [clojure.string :only [split-lines, split]]
+  [clojure.set :only [difference]]))
 
+;output the top 10 entries from cl into a file for comparison
 (defn spitlist
   [list]
+  (spit "test.txt" "")
   (doseq [i list]
     (spit "test.txt"
           (str (attribute i "data-pid") ",") :append true)
     (spit "test.txt"
           (str (text (find-element-under i {:css ".pl a"})) ","):append true)
     (spit "test.txt"
-         (str (text (find-element-under i {:css "span span span"})) "\n"):append true))
-    (print "Finished reading the top 10 bicycle entries"))
+         (str (text (find-element-under i {:css "span span span"})) "\n"):append true)))
 
+;read the top 10 post-ids and return them
 (defn readlist
   [file]
-  (doseq [j (split-lines (slurp file))]
-    (prn j)))
+  (for [j (split-lines (slurp file))]
+    (first (split j #","))))
+
+;compare the current cl post-ids to the ones we have logged
+;return the ones that do not match the previous top 10
+(defn findnew
+  [listings]
+  (let [pidmap
+        (zipmap
+          (for [i (take 10 listings)] (attribute i "data-pid"))
+          (take 10 listings))]
+        (doseq [i (difference
+          (set (keys pidmap))
+          (set (readlist "test.txt")))]
+          (seq (find pidmap i)))))
+
 
 (defn -main
   [& args]
     (set-driver! {:browser :firefox} "http://boston.craigslist.com/bia/")
     (let [listings (find-elements {:css ".row"})]
-      (spitlist (take 10 listings))))
+      (findnew listings)
+      (spitlist (take 10 listings)))
+    (quit))
 
 
